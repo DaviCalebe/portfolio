6bbeb0129f5d8fc84a25bc0c7382fcb7a06b660f
davicalebe <davicalebedon@gmail.com>
2025-02-17 19:21:08 -0300

    feat: introduction section

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,29 @@
 import './App.css';
 import { ToggleNav } from './components/toggle-nav';
+import brazilflag from './assets/brazil-flag.svg'
+import usaflag from './assets/usa-flag.svg'
 
 function App() {
 
   return (
     <main>
       <ToggleNav />
+      <div className="intro">
+        <div className="description">
+          <h1 id='name'>{"<"}Davi Calebe{"/>"}</h1>
+          <p className='occupation'>dev full-stack</p>
+
+          <div className="cv-buttons">
+            <button> <img src={brazilflag} alt="" /> CV</button>
+            <button><img src={usaflag} alt="" /> CV</button>
+          </div>
+        </div>
+        
+        <div className="profile-pic">
+          <div className="circle"></div>
+          <div className="circle-img"></div>
+        </div>
+      </div>
     </main>
   )
 }
