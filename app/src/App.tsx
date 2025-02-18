94c66451670949cc19f42f29cf41292de6d2ed8e
davicalebe <davicalebedon@gmail.com>
2025-02-18 16:04:41 -0300

    feat: about section and cv buttons' action

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { ToggleNav } from './components/toggle-nav';
 import brazilflag from './assets/brazil-flag.svg'
 import usaflag from './assets/usa-flag.svg'
+import element from './assets/element.svg'
 
 function App() {
 
@@ -14,8 +15,8 @@ function App() {
           <p className='occupation'>dev full-stack</p>
 
           <div className="cv-buttons">
-            <button> <img src={brazilflag} alt="" /> CV</button>
-            <button><img src={usaflag} alt="" /> CV</button>
+            <button onClick={() => window.location.href = "https://drive.google.com/uc?export=download&id=18FH_bWyRGCCaerdntXZsPgoWZsE0reJZ"}> <img src={brazilflag} alt="brazil-flag" /> CV</button>
+            <button onClick={() => window.location.href = "https://drive.google.com/uc?export=download&id=1_2R6twVKEHGEQ2Yp_zBaXo0XMPyIakZL"}><img src={usaflag} alt="usa-flag" /> CV</button>
           </div>
         </div>
         
@@ -24,6 +25,23 @@ function App() {
           <div className="circle-img"></div>
         </div>
       </div>
+
+      <section className="about">
+        <img src={element} alt="figma-element" />
+          <svg viewBox="0 0 900 900" className="target-svg"> 
+            <circle cx="450" cy="450" r="450" stroke="black" strokeWidth="2" fill="none" />
+            <circle cx="450" cy="450" r="360" stroke="black" strokeWidth="2" fill="none" />
+            <circle cx="450" cy="450" r="260" stroke="black" strokeWidth="2" fill="none" />
+            <circle cx="450" cy="450" r="190" stroke="black" strokeWidth="2" fill="none" />
+            <circle cx="450" cy="450" r="90" stroke="black" strokeWidth="2" fill="#AC9BBC" />
+            <circle cx="450" cy="450" r="20" fill="black" />
+          </svg>
+        <div className="about-content">
+        </div>
+        <img src={element} className='reversed-element' alt="figma-element" />
+      </section>
+
+
     </main>
   )
 }
